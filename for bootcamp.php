84583daa926c789a38011180028cd5dcee5bb275
NeilTadz <83717446+NeilTadz@users.noreply.github.com>
2021-08-04 12:57:31 +0800

    Add files via upload

---
+++ for bootcamp.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1> HI! Welcome to my BIO</h1>
+	<?php 
+		echo "My Name is Neil Gabriel Tadea<br>";
+		echo "I'm From Taguig<br>";
+		echo "I'm 19 years old<br>";
+		echo "My Favorite color is gray<br>";
+		echo "My Favorite Food is Lasagna<br>";
+		echo "I'm Incoming Second Year College<br>";
+		echo "I'm Studying in University of Makati<br>";
+	?>
+</body>
+</html>
